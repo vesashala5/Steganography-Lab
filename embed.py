75f3589a645de0e7e423f965eaf86b6fff1d49de
vesashala5 <vesa.shala5@student.uni-pr.edu>
2026-05-10 18:20:27 +0200

    update file

---
+++ embed.py
@@ -0,0 +1,10 @@
+from PIL import Image
+import random
+
+key = 2026
+colour_plane = 1
+bit_position = 7
+
+cover_image = 'img/flowers.bmp'
+secret_file = 'secret.txt'
+output_image = 'stego-image.bmp'
